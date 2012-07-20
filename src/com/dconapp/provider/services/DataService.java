a72d7aa121115cb0565bef4897e1d647f1c55d5e
Cody Stamps <crstamps2@gmail.com>
2012-07-19 23:27:43 -0400

    Hits service and writes to bucket
    The app can be successfully hit via POST from
    http://dconapp2012.appspot.com/dconapp2012/DataService/post.
    
    Writes json file to dconjson bucket.

--- src/com/dconapp/provider/services/DataService.java
+++ src/com/dconapp/provider/services/DataService.java
@@ -9,97 +9,100 @@ import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.nio.ByteBuffer;
 import java.nio.channels.Channels;
-import java.sql.SQLException;
 
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
-import com.dconapp.model.EventSlots;
-import com.dconapp.provider.dao.CommonSlotsDAO;
-import com.dconapp.provider.dao.wrappers.CommonSlotsDAOWrapper;
+
 import com.google.appengine.api.files.AppEngineFile;
 import com.google.appengine.api.files.FileService;
 import com.google.appengine.api.files.FileServiceFactory;
 import com.google.appengine.api.files.FileWriteChannel;
 import com.google.appengine.api.files.GSFileOptions.GSFileOptionsBuilder;
-import com.google.gson.Gson;
 
 /**
- * @author Cody
+ * @author
  *
  */
 @SuppressWarnings("serial")
-public class DataService extends HttpServlet {
+@Path("/DataService")
+public class DataService extends HttpServlet{
 	
+	@POST
+	@Produces("application/json")
+	@Consumes("application/json")
+	@Path("/post")
 	@Override
 	public void doPost(HttpServletRequest req, HttpServletResponse resp){
-
-		EventSlots events = null;
+		Response returnable = null;
 		try{
 		InputStreamReader input = new InputStreamReader(req.getInputStream());
 		BufferedReader reader = new BufferedReader(input);
-		CommonSlotsDAO dao = new CommonSlotsDAO();
-		try {
-			events = dao.executeQuery(new CommonSlotsDAOWrapper());
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		// Get the file service
+		String json = reader.readLine();
+//		System.out.println(reader.readLine());
+//		
+//		// Get the file service
 		FileService fileService = FileServiceFactory.getFileService();
-
-		/**
-		 * Set up properties of your new object
-		 * After finalizing objects, they are accessible
-		 * through Cloud Storage with the URL:
-		 * http://commondatastorage.googleapis.com/my_bucket/my_object
-		 */
+//
+//		/**
+//		 * Set up properties of your new object
+//		 * After finalizing objects, they are accessible
+//		 * through Cloud Storage with the URL:
+//		 * http://commondatastorage.googleapis.com/my_bucket/my_object
+//		 */
 		GSFileOptionsBuilder optionsBuilder = new GSFileOptionsBuilder()
 		  .setBucket("dconjson")
-		  .setKey("json")
 		  .setAcl("public-read")
+		  .setKey("json")
 		  .setMimeType("application/json");
-
-		// Create your object
+//
+//		// Create your object
 		AppEngineFile writableFile = fileService.createNewGSFile(optionsBuilder.build());
-		
-		// Open a channel for writing
+//		
+//		// Open a channel for writing
 		boolean lockForWrite = false; // Do you want to exclusively lock this object?
 		FileWriteChannel writeChannel = fileService.openWriteChannel(writableFile, lockForWrite);
-		
-		// For this example, we write to the object using the PrintWriter
+//		
+//		// For this example, we write to the object using the PrintWriter
 		PrintWriter out = new PrintWriter(Channels.newWriter(writeChannel, "UTF8"));
-		out.println(reader.readLine());
-
-		// Close the object without finalizing.
+		out.println(json);
+//		out.flush();
+////		// Close the object without finalizing.
 		out.close();
-
-		/**
-		 * You can write to this file again, later, by saving the file path.
-		 * It is not possible to read the file until you finalize it.
-		 * Once you finalize a file, it is not possible to write to it.
-		 */
-
-		// Save the file path
+//
+//		/**
+//		 * You can write to this file again, later, by saving the file path.
+//		 * It is not possible to read the file until you finalize it.
+//		 * Once you finalize a file, it is not possible to write to it.
+//		 */
+//
+//		// Save the file path
 		String path = writableFile.getFullPath();
-
-		// Lock the file so no one else can access it at the same time
+//
+//		// Lock the file so no one else can access it at the same time
 		lockForWrite = true;
 
 		// Write to the unfinalized file again in a separate request
 		writableFile = new AppEngineFile(path);
 		writeChannel = fileService.openWriteChannel(writableFile, lockForWrite);
-		writeChannel.write(ByteBuffer.wrap("And miles to go before I sleep.".getBytes()));
-		
-		// Finalize the object
+//		writeChannel.write(ByteBuffer.wrap(json.getBytes()));
+//		
+//		// Finalize the object
 		writeChannel.closeFinally();
-		
-		Gson gson = new Gson();
-		String json = gson.toJson(events);
-		System.out.println(json);
+		//returnable = Response.ok().entity("{\"Message\":\"Hello World\"}").build();
+		resp.setStatus(HttpServletResponse.SC_OK);
+		resp.setHeader("content-type", "application/json");
+		PrintWriter respWriter = resp.getWriter();
+		respWriter.println("{\"Message\":\"Hello World\"}");
 		}
-		
 		catch (IOException ioe){
 			System.out.println(ioe.getMessage());
 		}
